fad9be26617da1d1764532b787590aa6967b7c49
logge <logge@optimal-systems.de>
2024-05-16 10:22:57 +0200

    added mdx loader to webpack config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,29 @@
 import createMDX from '@next/mdx'
 import rehypeHighlight from "rehype-highlight";
 
+/** @type {import('webpack').Configuration} */
+const webpackConfig = {
+    module: {
+        rules: [
+            {
+                test: /\.mdx?$/,
+                use: [
+                    {
+                        loader: '@mdx-js/loader',
+                        /** @type {import('@mdx-js/loader').Options} */
+                        options: {}
+                    }
+                ]
+            }
+        ]
+    }
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
-    reactStrictMode: true
+    reactStrictMode: true,
+    webpack: webpackConfig
 }
 
 const withMDX = createMDX({
