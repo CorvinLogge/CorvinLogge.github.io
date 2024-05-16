75937bbc6c92eaac7e8bd06679715c66594e5eb0
logge <logge@optimal-systems.de>
2024-05-16 16:21:21 +0200

    added a media query to test it

--- src/app/projects/layout.tsx
+++ src/app/projects/layout.tsx
@@ -7,13 +7,13 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <>
+        <div className={style.ProjectPageContainer}>
             <input type="checkbox" id="preview-checkbox" className={style.previewCheckbox} defaultChecked={true}/>
             <label htmlFor="preview-checkbox" className={style.previewButton}/>
 
             <div className={style.ProjectPageRoot}>
                 {children}
             </div>
-        </>
+        </div>
     )
 }
